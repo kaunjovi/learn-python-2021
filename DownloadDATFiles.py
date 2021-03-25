2b5f19d5a3d5f9324c3a5dee0a6af854baa1da72
kaun jovi <kaunjovi@gmail.com>
2021-03-25 23:39:39 +0530

    download MTO data files from NSE

---
+++ DownloadDATFiles.py
@@ -0,0 +1,23 @@
+import urllib.request
+import shutil
+
+# download file from url 
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    file_name = "./raw-data-folder/" + local_filename
+
+    ## write something in the file. 
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+# download file based on the name of the file from nse 
+def download_mto_data(mto_file_name) : 
+    download_file ("https://archives.nseindia.com/archives/equities/mto/" + mto_file_name + ".DAT")    
+
+
+# download MTO files for given dates 
+
+download_mto_data ("MTO_22032021")
+download_mto_data ("MTO_23032021")
+download_mto_data ("MTO_24032021")
+download_mto_data ("MTO_25032021")
