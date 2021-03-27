b2c84b0b7ea23a92e03f6460db5967b84559b7bd
kaun jovi <kaunjovi@gmail.com>
2021-03-27 10:39:10 +0530

    from raw MTO, get equity only lines

--- src/PullEQOnlyData.py
+++ src/PullEQOnlyData.py
@@ -1,16 +1,32 @@
+import os
 import re
+
+def fetchDateFromMTODataFileName (fileName):
+    date = fileName[4:6]
+    month = fileName[6:8]
+    year = fileName[8:12]
+    return year + month +  date 
+
+def modifyDataLine ( rawLine , date ): 
+    return re.sub(r"^[0-9]*,[0-9]*,", date + "," , rawLine )
 
 # Pull EQ only data from the MTO data files 
 def pull_equity_only_data(file ):
     pattern = re.compile("^.*(,EQ,).*$")
+    date = fetchDateFromMTODataFileName(file)
+    with open("./eq-only-data-folder/" + file, 'w') as equity_only_file:
+        for i, line in enumerate(open("./raw-data-folder/" + file )):
+            for match in re.finditer(pattern, line):
+                equityLine = match.group()
+                modifiedLine = modifyDataLine (equityLine, date)
+                # print (modifiedLine)
+                equity_only_file.write(modifiedLine + "\n")
 
-    for i, line in enumerate(open("./raw-data-folder/" + file + ".DAT")):
-        for match in re.finditer(pattern, line):
-            print (match.group())
 
+def prepareRawMTODataForAnalysis (folder) : 
+    for filename in os.listdir(folder):
+        if filename.endswith(".DAT") : 
+            # print(filename)
+            pull_equity_only_data(filename)
 
-pull_equity_only_data ("MTO_15032021")
-
-
-## getRawMTOData 
-## prepareEQMTOData 
+prepareRawMTODataForAnalysis ("./raw-data-folder/")
