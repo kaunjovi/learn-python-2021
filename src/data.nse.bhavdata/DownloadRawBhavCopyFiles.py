621673b4d3af4352caf65c4f788d7871e17f8104
kaun jovi <kaunjovi@gmail.com>
2021-03-29 18:31:34 +0530

    Move the analysis code to its own file.

--- src/data.nse.bhavdata/DownloadRawBhavCopyFiles.py
+++ src/data.nse.bhavdata/DownloadRawBhavCopyFiles.py
@@ -1,7 +1,5 @@
 import urllib.request
 import shutil
-import pandas as pd
-from glob import glob
 
 DATA_URL:str = 'https://archives.nseindia.com/products/content/'
 BHAV_DATA_FILE_FIRSTNAME:str = 'sec_bhavdata_full_'
@@ -48,47 +46,3 @@ def download_bhavcopy_from_date (date:str):
 # download_bhavcopy_from_date ("25032021")
 # download_bhavcopy_from_date ('26032021')
 
-## Now lets do some analytics on it. 
-
-# I would like to look at the entire dataset when I print out 
-pd.set_option('display.max_rows', 2000)
-pd.set_option('display.float_format', '{:,.2f}'.format)
-
-filenames = glob(RAW_BHAV_DATA_FOLDER + '*.csv')
-dataframes = [pd.read_csv(f) for f in filenames]
-df = pd.concat( dataframes)
-df.rename(columns=lambda x: x.strip(), inplace=True)
-df.fillna(0)
-
-## Too much data. 
-## Drop a few columns for the time. 
-df.drop(['OPEN_PRICE'], axis=1 , inplace=True)
-df.drop(['HIGH_PRICE'], axis=1,  inplace=True)
-df.drop(['LOW_PRICE'], axis=1, inplace=True)
-df.drop(['LAST_PRICE'], axis=1, inplace=True)
-
-## DELIV_PER has some '-' in it. Fix them. Fix the column type. 
-df['DELIV_PER'] = df['DELIV_PER'].str.strip()
-df['DELIV_PER'] = df['DELIV_PER'].replace(['-'],'0.00')
-df[['DELIV_PER']] = df[['DELIV_PER']].apply(pd.to_numeric)
-
-## DELIV_QTY has some '-' in it. Fix them. Fix the column type. 
-df['DELIV_QTY'] = df['DELIV_QTY'].str.strip()
-df['DELIV_QTY'] = df['DELIV_QTY'].replace(['-'],'0')
-df[['DELIV_QTY']] = df[['DELIV_QTY']].apply(pd.to_numeric)
-
-
-
-
-# for col in grouped_df.columns:
-#     print(col)
-
-# grouped_df = df.groupby(['SYMBOL'])[['TURNOVER_LACS', 'NO_OF_TRADES']].mean()
-# # sorted_df = grouped_df.sort_values( ['TURNOVER_LACS', 'NO_OF_TRADES' ], ascending = [False,False])
-
-# for col in grouped_df.columns:
-#     print(col)
-
-grouped_df = df.groupby(['SYMBOL'])[['TURNOVER_LACS', 'DELIV_PER']].mean()
-sorted_df = grouped_df.sort_values( ['TURNOVER_LACS', 'DELIV_PER' ], ascending = [False,False])
-print (sorted_df.head(50))
